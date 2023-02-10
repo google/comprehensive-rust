37f6daf797d46b9cd0d31892a1e96e7e8c70240d
Charisee Chiw <charisee.chiw@gmail.com>
2023-02-10 12:23:53 -0800

    Update health-statistics.rs

--- third_party/rust-on-exercism/health-statistics.rs
+++ third_party/rust-on-exercism/health-statistics.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct User {
     name: String,
     age: u32,
@@ -5,27 +6,27 @@ struct User {
 }
 
 impl User {
-    pub fn new(name: String, age: u32, weight: f32) -> Self {
-        unimplemented!()
-    }
+    //pub fn new(name: String, age: u32, weight: f32) -> Self {
+    //    unimplemented!()
+    //}
 
-    pub fn name(&self) -> &str {
-        unimplemented!()
-    }
+    //pub fn name(&self) -> &str {
+    //    unimplemented!()
+    //}
 
-    pub fn age(&self) -> u32 {
-        unimplemented!()
-    }
+    //pub fn age(&self) -> u32 {
+    //    unimplemented!()
+    //}
 
-    pub fn weight(&self) -> f32 {
-        unimplemented!()
-    }
+    //pub fn weight(&self) -> f32 {
+    //    unimplemented!()
+    //}
 
-    pub fn set_age(&mut self, new_age: u32) {
-        unimplemented!()
-    }
+    //pub fn set_age(&mut self, new_age: u32) {
+    //    unimplemented!()
+    //}
 
-    pub fn set_weight(&mut self, new_weight: f32) {
-        unimplemented!()
-    }
+    //pub fn set_weight(&mut self, new_weight: f32) {
+    //    unimplemented!()
+    //}
 }
