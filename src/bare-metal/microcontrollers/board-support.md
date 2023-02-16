b4c93f08e484d2d59f0824eb3de7f791dfc7aee9
Andrew Walbran <qwandor@google.com>
2023-02-16 00:49:43 +0000

    Add board support crate example.

--- src/bare-metal/microcontrollers/board-support.md
+++ src/bare-metal/microcontrollers/board-support.md
@@ -1,1 +1,17 @@
 # Board support crates
+
+Board support crates provide a further level of wrapping for a specific board for convenience.
+
+```rust,editable,compile_fail
+{{#include examples/src/bin/board_support.rs:Example}}
+```
+
+<details>
+
+ * In this case the board support crate is just providing more useful names, and a bit of
+   initialisation.
+ * The crate may also include drivers for some on-board devices outside of the microcontroller
+   itself.
+   * `microbit-v2` includes a simple driver for the LED matrix.
+
+</details>
---
+++ src/bare-metal/microcontrollers/examples/src/bin/board_support.rs
@@ -0,0 +1,32 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// ANCHOR: Example
+#![no_main]
+#![no_std]
+
+extern crate panic_halt as _;
+
+use cortex_m_rt::entry;
+use microbit::{hal::prelude::*, Board};
+
+#[entry]
+fn main() -> ! {
+    let mut board = Board::take().unwrap();
+
+    board.display_pins.col1.set_low().unwrap();
+    board.display_pins.row1.set_high().unwrap();
+
+    loop {}
+}
