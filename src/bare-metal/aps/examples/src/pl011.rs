8c852ff756e27ebf54531c070e6b248a57808572
Andrew Walbran <qwandor@google.com>
2023-03-07 16:54:59 +0000

    QEMU virt machine uses PL011 UART not 16550.

--- src/bare-metal/aps/examples/src/pl011.rs
+++ src/bare-metal/aps/examples/src/pl011.rs
@@ -13,24 +13,25 @@
 // limitations under the License.
 
 // ANCHOR: Example
-use core::ptr::write_volatile;
+const FLAG_REGISTER_OFFSET: usize = 0x18;
+const FR_BUSY: u8 = 1 << 3;
+const FR_TXFF: u8 = 1 << 5;
 
-/// Minimal driver for an 8250 UART. This only implements enough to work with
-/// the emulated 8250 provided by crosvm, and won't work with real hardware.
+/// Minimal driver for a PL011 UART.
 #[derive(Debug)]
 pub struct Uart {
     base_address: *mut u8,
 }
 
 impl Uart {
-    /// Constructs a new instance of the UART driver for a device at the given
-    /// base address.
+    /// Constructs a new instance of the UART driver for a PL011 device at the
+    /// given base address.
     ///
     /// # Safety
     ///
-    /// The given base address must point to the 8 MMIO control registers of an
-    /// appropriate UART device, which must be mapped into the address space of
-    /// the process as device memory and not have any other aliases.
+    /// The given base address must point to the 8 MMIO control registers of a
+    /// PL011 device, which must be mapped into the address space of the process
+    /// as device memory and not have any other aliases.
     pub unsafe fn new(base_address: usize) -> Self {
         Self {
             base_address: base_address as *mut u8,
@@ -39,11 +40,24 @@ impl Uart {
 
     /// Writes a single byte to the UART.
     pub fn write_byte(&self, byte: u8) {
+        // Wait until there is room in the TX buffer.
+        while self.read_flag_register() & FR_TXFF != 0 {}
+
         // Safe because we know that the base address points to the control
-        // registers of a UART device which is appropriately mapped.
+        // registers of a PL011 device which is appropriately mapped.
         unsafe {
-            write_volatile(self.base_address, byte);
+            // Write to the TX buffer.
+            self.base_address.write_volatile(byte);
         }
+
+        // Wait until the UART is no longer busy.
+        while self.read_flag_register() & FR_BUSY != 0 {}
+    }
+
+    fn read_flag_register(&self) -> u8 {
+        // Safe because we know that the base address points to the control
+        // registers of a PL011 device which is appropriately mapped.
+        unsafe { self.base_address.add(FLAG_REGISTER_OFFSET).read_volatile() }
     }
 }
 // ANCHOR_END: Example
