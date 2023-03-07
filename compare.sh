effed3981ff827318896e3f910f46e0ffeed7be4
Dustin J. Mitchell <djmitche@google.com>
2023-03-07 17:47:03 +0000

    remove temporary code

--- compare.sh
+++
@@ -1,6 +0,0 @@
-rm -rf rendered
-mkdir -p rendered
-for lang in ko da de; do
-  MDBOOK_BOOK__LANGUAGE=$lang-new MDBOOK_OUTPUT='{"markdown": {}}' mdbook build -d rendered/$lang-new
-  diff -u --color -r rendered/$lang-new expected/$lang-expected
-done
