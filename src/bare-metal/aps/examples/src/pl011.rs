31353983eb72aed0e0b13bb57806a7d0e690b2bd
Andrew Walbran <qwandor@google.com>
2023-03-13 11:57:45 +0000

    Write a more complete UART driver.

--- src/bare-metal/aps/examples/src/pl011.rs
+++ src/bare-metal/aps/examples/src/pl011.rs
@@ -12,15 +12,96 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// ANCHOR: Example
-const FLAG_REGISTER_OFFSET: usize = 0x18;
-const FR_BUSY: u8 = 1 << 3;
-const FR_TXFF: u8 = 1 << 5;
+use core::{
+    fmt::{self, Write},
+    ptr::{addr_of, addr_of_mut},
+};
 
-/// Minimal driver for a PL011 UART.
+// ANCHOR: Flags
+use bitflags::bitflags;
+
+bitflags! {
+    /// Flags from the UART flag register.
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+    struct Flags: u16 {
+        /// Clear to send.
+        const CTS = 1 << 0;
+        /// Data set ready.
+        const DSR = 1 << 1;
+        /// Data carrier detect.
+        const DCD = 1 << 2;
+        /// UART busy transmitting data.
+        const BUSY = 1 << 3;
+        /// Receive FIFO is empty.
+        const RXFE = 1 << 4;
+        /// Transmit FIFO is full.
+        const TXFF = 1 << 5;
+        /// Receive FIFO is full.
+        const RXFF = 1 << 6;
+        /// Transmit FIFO is empty.
+        const TXFE = 1 << 7;
+        /// Ring indicator.
+        const RI = 1 << 8;
+    }
+}
+// ANCHOR_END: Flags
+
+bitflags! {
+    /// Flags from the UART Receive Status Register / Error Clear Register.
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
+    struct ReceiveStatus: u16 {
+        /// Framing error.
+        const FE = 1 << 0;
+        /// Parity error.
+        const PE = 1 << 1;
+        /// Break error.
+        const BE = 1 << 2;
+        /// Overrun error.
+        const OE = 1 << 3;
+    }
+}
+
+// ANCHOR: Registers
+#[repr(C, align(4))]
+struct Registers {
+    dr: u16,
+    _reserved0: [u8; 2],
+    rsr: ReceiveStatus,
+    _reserved1: [u8; 19],
+    fr: Flags,
+    _reserved2: [u8; 6],
+    ilpr: u8,
+    _reserved3: [u8; 3],
+    ibrd: u16,
+    _reserved4: [u8; 2],
+    fbrd: u8,
+    _reserved5: [u8; 3],
+    lcr_h: u8,
+    _reserved6: [u8; 3],
+    cr: u16,
+    _reserved7: [u8; 3],
+    ifls: u8,
+    _reserved8: [u8; 3],
+    imsc: u16,
+    _reserved9: [u8; 2],
+    ris: u16,
+    _reserved10: [u8; 2],
+    mis: u16,
+    _reserved11: [u8; 2],
+    icr: u16,
+    _reserved12: [u8; 2],
+    dmacr: u8,
+    _reserved13: [u8; 3],
+}
+// ANCHOR_END: Registers
+
+// ANCHOR: Uart
+/// Driver for a PL011 UART.
 #[derive(Debug)]
 pub struct Uart {
-    base_address: *mut u8,
+    registers: *mut Registers,
 }
 
 impl Uart {
@@ -32,38 +113,46 @@ impl Uart {
     /// The given base address must point to the 8 MMIO control registers of a
     /// PL011 device, which must be mapped into the address space of the process
     /// as device memory and not have any other aliases.
-    pub unsafe fn new(base_address: usize) -> Self {
+    pub unsafe fn new(base_address: *mut u32) -> Self {
         Self {
-            base_address: base_address as *mut u8,
+            registers: base_address as *mut Registers,
         }
     }
 
     /// Writes a single byte to the UART.
     pub fn write_byte(&self, byte: u8) {
         // Wait until there is room in the TX buffer.
-        while self.read_flag_register() & FR_TXFF != 0 {}
+        while self.read_flag_register().contains(Flags::TXFF) {}
 
-        // Safe because we know that the base address points to the control
+        // Safe because we know that self.registers points to the control
         // registers of a PL011 device which is appropriately mapped.
         unsafe {
             // Write to the TX buffer.
-            self.base_address.write_volatile(byte);
+            addr_of_mut!((*self.registers).dr).write_volatile(byte.into());
         }
 
         // Wait until the UART is no longer busy.
-        while self.read_flag_register() & FR_BUSY != 0 {}
+        while self.read_flag_register().contains(Flags::BUSY) {}
     }
 
-    fn read_flag_register(&self) -> u8 {
-        // Safe because we know that the base address points to the control
+    /// Reads and returns a pending byte, or `None` if nothing has been received.
+    pub fn read_byte(&self) -> Option<u8> {
+        if self.read_flag_register().contains(Flags::RXFE) {
+            None
+        } else {
+            let data = unsafe { addr_of!((*self.registers).dr).read_volatile() };
+            // TODO: Check for error conditions in bits 8-11.
+            Some(data as u8)
+        }
+    }
+
+    fn read_flag_register(&self) -> Flags {
+        // Safe because we know that self.registers points to the control
         // registers of a PL011 device which is appropriately mapped.
-        unsafe { self.base_address.add(FLAG_REGISTER_OFFSET).read_volatile() }
+        unsafe { addr_of!((*self.registers).fr).read_volatile() }
     }
 }
-// ANCHOR_END: Example
-
-// ANCHOR: Traits
-use core::fmt::{self, Write};
+// ANCHOR_END: Uart
 
 impl Write for Uart {
     fn write_str(&mut self, s: &str) -> fmt::Result {
@@ -77,4 +166,3 @@ impl Write for Uart {
 // Safe because it just contains a pointer to device memory, which can be
 // accessed from any context.
 unsafe impl Send for Uart {}
-// ANCHOR_END: Traits
