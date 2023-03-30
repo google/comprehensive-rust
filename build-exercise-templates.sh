06badccb197653ac6f1266f0b66967cfe3f98c84
Andrew Walbran <qwandor@google.com>
2023-03-30 15:37:05 +0100

    Remove output before building exercises.

--- build-exercise-templates.sh
+++ build-exercise-templates.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+rm -rf exercise-templates/ book/exercises.zip
+
 mkdir exercise-templates
 cargo run --bin exerciser src/exercises/bare-metal/compass.md exercise-templates/compass
 cargo run --bin exerciser src/exercises/bare-metal/rtc.md exercise-templates/rtc
