8844601edea2291d9aae107798f7e722b33c8ff2
Andrew Walbran <qwandor@google.com>
2023-03-30 15:47:38 +0100

    Ensure that book directory exists.

--- build-exercise-templates.sh
+++ build-exercise-templates.sh
@@ -8,4 +8,5 @@ mkdir exercise-templates
 cargo run --bin exerciser src/exercises/bare-metal/compass.md exercise-templates/compass
 cargo run --bin exerciser src/exercises/bare-metal/rtc.md exercise-templates/rtc
 
+mkdir -p book
 zip --recurse-paths book/exercises.zip exercise-templates/
