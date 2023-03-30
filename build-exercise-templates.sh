97d3c9c17d9b123490835bf31c559ffa2d68b7fc
Andrew Walbran <qwandor@google.com>
2023-03-30 13:22:29 +0100

    Add script to run exerciser on bare metal exercises.

---
+++ build-exercise-templates.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+mkdir exercise-templates
+cargo run --bin exerciser src/exercises/bare-metal/compass.md exercise-templates/compass
+cargo run --bin exerciser src/exercises/bare-metal/rtc.md exercise-templates/rtc
