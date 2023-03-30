3f7cc60686dda65f6a05e1a073728a46ad654bac
Andrew Walbran <qwandor@google.com>
2023-03-30 15:35:30 +0100

    Make zip part as part of script.
    This makes running local builds easier.

--- build-exercise-templates.sh
+++ build-exercise-templates.sh
@@ -5,3 +5,5 @@ set -euxo pipefail
 mkdir exercise-templates
 cargo run --bin exerciser src/exercises/bare-metal/compass.md exercise-templates/compass
 cargo run --bin exerciser src/exercises/bare-metal/rtc.md exercise-templates/rtc
+
+zip --recurse-paths book/exercises.zip exercise-templates/
