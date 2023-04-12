2f442acda75b5645450fc515d5ccedbefb890abc
Andrew Walbran <qwandor@google.com>
2023-04-12 16:35:11 +0100

    Silence warnings about unused methods in provided drivers.

--- src/exercises/bare-metal/rtc/src/pl011.rs
+++ src/exercises/bare-metal/rtc/src/pl011.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![allow(unused)]
+
 use core::fmt::{self, Write};
 use core::ptr::{addr_of, addr_of_mut};
 
