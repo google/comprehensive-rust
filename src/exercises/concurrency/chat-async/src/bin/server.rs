e19864d387dab1e993bd3be1eab38b0dfbdb7c14
Dustin J. Mitchell <djmitche@google.com>
2023-05-10 21:43:26 +0000

    really basic solution

--- src/exercises/concurrency/chat-async/src/bin/server.rs
+++ src/exercises/concurrency/chat-async/src/bin/server.rs
@@ -2,6 +2,7 @@ use futures_util::sink::SinkExt;
 use std::net::SocketAddr;
 use thiserror::Error;
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::broadcast::{Sender, channel, error::{SendError, RecvError}};
 use tokio_websockets::{ServerBuilder, WebsocketStream, Message};
 
 #[derive(Error, Debug)]
@@ -10,6 +11,10 @@ enum ServerError {
     Websocket(String),
     #[error("io error: {0}")]
     IO(#[from] std::io::Error),
+    #[error("broadcast channel SendError: {0}")]
+    SendError(#[from] SendError<String>),
+    #[error("broadcast channel RecvError: {0}")]
+    RecvError(#[from] RecvError),
 }
 
 // tokio_websockets Error types do not implement std::error::Error, so we make do by just capturing
@@ -29,19 +34,25 @@ impl From<tokio_websockets::proto::ProtocolError> for ServerError {
 async fn handle_connection(
     addr: SocketAddr,
     mut ws_stream: WebsocketStream<TcpStream>,
+    bcast_tx: Sender<String>,
 ) -> Result<(), ServerError> {
     ws_stream.send(Message::text("Welcome to chat! Type a message".into())).await?;
+    let mut bcast_rx = bcast_tx.subscribe();
     loop {
         tokio::select! {
             incoming = ws_stream.next() => {
                 match incoming {
                     Some(Ok(msg)) => {
-                        println!("From client {addr:?} {:?}", msg.as_text()?);
-                        // TODO: broadcast the message to other connected clients
+                        let msg = msg.as_text()?;
+                        println!("From client {addr:?} {msg:?}");
+                        bcast_tx.send(msg.into())?;
                     }
                     Some(Err(err)) => return Err(err.into()),
                     None => return Ok(()),
                 }
+            }
+            msg = bcast_rx.recv() => {
+                ws_stream.send(Message::text(msg?)).await?;
             }
         }
     }
@@ -49,17 +60,20 @@ async fn handle_connection(
 
 #[tokio::main]
 async fn main() -> Result<(), ServerError> {
+    let (bcast_tx, _) = channel(16);
+
     let listener = TcpListener::bind("127.0.0.1:2000").await?;
     println!("listening on port 2000");
 
     loop {
         let (socket, addr) = listener.accept().await?;
         println!("New connection from {addr:?}");
+        let bcast_tx = bcast_tx.clone();
         tokio::spawn(async move {
             // Wrap the raw TCP stream into a websocket.
             let ws_stream = ServerBuilder::new().accept(socket).await?;
 
-            handle_connection(addr, ws_stream).await
+            handle_connection(addr, ws_stream, bcast_tx).await
         });
     }
 }
