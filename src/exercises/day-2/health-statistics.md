74ed19339dc1d104d8490eb2d42be8a4b714bc06
Frances Wingerter <fw@immunant.com>
2023-07-05 18:58:18 -0400

    exercises: health-statistics: add health report
    this lets us see a non-setter use case for &mut self
    
    it also makes the 'statistics' side of this exercise more explicit as we count doctor visits

--- src/exercises/day-2/health-statistics.md
+++ src/exercises/day-2/health-statistics.md
@@ -29,4 +29,25 @@ fn test_set_age() {
     bob.set_age(33);
     assert_eq!(bob.age(), 33);
 }
+
+#[test]
+fn test_visit() {
+    let mut bob = User::new(String::from("Bob"), 32, 155.2);
+    assert_eq!(bob.doctor_visits(), 0);
+    let report = bob.visit_doctor(Measurements {
+        height: 156.1,
+        blood_pressure: (120, 80),
+    });
+    assert_eq!(report.patient_name, "Bob");
+    assert_eq!(report.visit_count, 1);
+    assert_eq!(report.blood_pressure_change, None);
+
+    let report = bob.visit_doctor(Measurements {
+        height: 156.1,
+        blood_pressure: (115, 76),
+    });
+
+    assert_eq!(report.visit_count, 2);
+    assert_eq!(report.blood_pressure_change, Some((-5, -4)));
+}
 ```
--- third_party/rust-on-exercism/health-statistics.rs
+++ third_party/rust-on-exercism/health-statistics.rs
@@ -2,6 +2,20 @@ pub struct User {
     name: String,
     age: u32,
     height: f32,
+    doctor_visits: usize,
+    last_blood_pressure: Option<(u32, u32)>,
+}
+
+pub struct Measurements {
+    height: f32,
+    blood_pressure: (u32, u32),
+}
+
+pub struct HealthReport<'a> {
+    patient_name: &'a str,
+    visit_count: u32,
+    height_change: f32,
+    blood_pressure_change: Option<(i32, i32)>,
 }
 
 impl User {
@@ -21,11 +35,19 @@ impl User {
         unimplemented!()
     }
 
+    pub fn doctor_visits(&self) -> u32 {
+        unimplemented!()
+    }
+
     pub fn set_age(&mut self, new_age: u32) {
         unimplemented!()
     }
 
     pub fn set_height(&mut self, new_height: f32) {
+        unimplemented!()
+    }
+
+    pub fn visit_doctor(&mut self, measurements: Measurements) -> HealthReport {
         unimplemented!()
     }
 }
