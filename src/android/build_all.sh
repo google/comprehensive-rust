c564ca625ff598f309e2f749274d364dfab5b140
Sharun <715417+sharunkumar@users.noreply.github.com>
2023-07-12 10:31:16 -0400

    remove $ from code-blocks in build_all.sh (#955)
    * removed $ from code-blocks in build_all.sh
    
    * separate the code from the output
    
    * added shell indicator for the code block
    
    * added shell indicator for the code block
    
    * update logging.md
    
    * separate the code from the output in other files as well
    
    * update the output code-blocks to text

--- src/android/build_all.sh
+++ src/android/build_all.sh
@@ -34,26 +34,26 @@ adb shell rm -rf '/data/local/tmp/*'
 
 run_example <<EOF
 # ANCHOR: hello_rust
-$ m hello_rust
-$ adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust /data/local/tmp"
-$ adb shell /data/local/tmp/hello_rust
+m hello_rust
+adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust /data/local/tmp"
+adb shell /data/local/tmp/hello_rust
 # ANCHOR_END: hello_rust
 EOF
 
 run_example <<EOF
 # ANCHOR: hello_rust_with_dep
-$ m hello_rust_with_dep
-$ adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust_with_dep /data/local/tmp"
-$ adb shell /data/local/tmp/hello_rust_with_dep
+m hello_rust_with_dep
+adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust_with_dep /data/local/tmp"
+adb shell /data/local/tmp/hello_rust_with_dep
 # ANCHOR_END: hello_rust_with_dep
 EOF
 
 function birthday_server() {
   run_example <<EOF
 # ANCHOR: birthday_server
-$ m birthday_server
-$ adb push "$ANDROID_PRODUCT_OUT/system/bin/birthday_server /data/local/tmp"
-$ adb shell /data/local/tmp/birthday_server
+m birthday_server
+adb push "$ANDROID_PRODUCT_OUT/system/bin/birthday_server /data/local/tmp"
+adb shell /data/local/tmp/birthday_server
 # ANCHOR_END: birthday_server
 EOF
 }
@@ -70,21 +70,21 @@ echo "Found birthdayservice..."
 
 run_example <<EOF
 # ANCHOR: service_check_birthday_server
-$ adb shell service check birthdayservice
+adb shell service check birthdayservice
 # ANCHOR_END: service_check_birthday_server
 EOF
 
 run_example <<EOF
 # ANCHOR: service_call_birthday_server
-$ adb shell service call birthdayservice 1 s16 Bob i32 24
+adb shell service call birthdayservice 1 s16 Bob i32 24
 # ANCHOR_END: service_call_birthday_server
 EOF
 
 run_example <<EOF
 # ANCHOR: birthday_client
-$ m birthday_client
-$ adb push "$ANDROID_PRODUCT_OUT/system/bin/birthday_client /data/local/tmp"
-$ adb shell /data/local/tmp/birthday_client Charlie 60
+m birthday_client
+adb push "$ANDROID_PRODUCT_OUT/system/bin/birthday_client /data/local/tmp"
+adb shell /data/local/tmp/birthday_client Charlie 60
 # ANCHOR_END: birthday_client
 EOF
 
@@ -93,38 +93,38 @@ pkill -f birthday_server
 
 run_example <<EOF
 # ANCHOR: hello_rust_logs
-$ m hello_rust_logs
-$ adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust_logs /data/local/tmp"
-$ adb shell /data/local/tmp/hello_rust_logs
+m hello_rust_logs
+adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust_logs /data/local/tmp"
+adb shell /data/local/tmp/hello_rust_logs
 # ANCHOR_END: hello_rust_logs
 EOF
 
 run_example <<EOF
 # ANCHOR: print_birthday_card
-$ m print_birthday_card
-$ adb push "$ANDROID_PRODUCT_OUT/system/bin/print_birthday_card /data/local/tmp"
-$ adb shell /data/local/tmp/print_birthday_card
+m print_birthday_card
+adb push "$ANDROID_PRODUCT_OUT/system/bin/print_birthday_card /data/local/tmp"
+adb shell /data/local/tmp/print_birthday_card
 # ANCHOR_END: print_birthday_card
 EOF
 
 run_example <<EOF
 # ANCHOR: libbirthday_bindgen_test
-$ atest libbirthday_bindgen_test
+atest libbirthday_bindgen_test
 # ANCHOR_END: libbirthday_bindgen_test
 EOF
 
 run_example <<EOF
 # ANCHOR: analyze_numbers
-$ m analyze_numbers
-$ adb push "$ANDROID_PRODUCT_OUT/system/bin/analyze_numbers /data/local/tmp"
-$ adb shell /data/local/tmp/analyze_numbers
+m analyze_numbers
+adb push "$ANDROID_PRODUCT_OUT/system/bin/analyze_numbers /data/local/tmp"
+adb shell /data/local/tmp/analyze_numbers
 # ANCHOR_END: analyze_numbers
 EOF
 
 run_example <<EOF
 # ANCHOR: helloworld_jni
-$ m helloworld_jni
-$ adb sync  # requires adb root && adb remount
-$ adb shell /system/bin/helloworld_jni
+m helloworld_jni
+adb sync  # requires adb root && adb remount
+adb shell /system/bin/helloworld_jni
 # ANCHOR_END: helloworld_jni
 EOF
