8623c5f58bc9ee9aae97332751094e371e0e6b17
Nick Radcliffe <njr@StochasticSolutions.com>
2023-10-03 10:39:13 +0100

    Remove callback, which was confusing

--- src/exercises/day-3/simple-gui.rs
+++ src/exercises/day-3/simple-gui.rs
@@ -43,14 +43,14 @@ impl Label {
 
 pub struct Button {
     label: Label,
-    callback: Box<dyn FnMut()>,
+    // callback: Box<dyn FnMut()>, // Would also take a callback
 }
 
 impl Button {
-    fn new(label: &str, callback: Box<dyn FnMut()>) -> Button {
+    fn new(label: &str) -> Button {  // would also take a callback
         Button {
             label: Label::new(label),
-            callback,
+            // would store the callback here
         }
     }
 }
@@ -158,8 +158,8 @@ fn main() {
     let mut window = Window::new("Rust GUI Demo 1.23");
     window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
     window.add_widget(Box::new(Button::new(
-        "Click me!",
-        Box::new(|| println!("You clicked the button!")),
+        "Click me!"
+        // Box::new(|context| { /* process the button press using context */) }),
     )));
     window.draw();
 }
