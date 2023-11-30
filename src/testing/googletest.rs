8862ab65302398001544bd3ab837f4acd833e0db
Martin Geisler <mgeisler@google.com>
2023-11-30 14:57:00 +0100

    Add slide on GoogleTest (#1528)
    We expect Android engineers to start using this crate going forward.

---
+++ src/testing/googletest.rs
@@ -0,0 +1,25 @@
+// ANCHOR: test_elements_are
+use googletest::prelude::*;
+
+#[googletest::test]
+fn test_elements_are() {
+    let value = vec!["foo", "bar", "baz"];
+    expect_that!(value, elements_are!(eq("foo"), lt("xyz"), starts_with("b")));
+}
+// ANCHOR_END: test_elements_are
+
+#[should_panic]
+// ANCHOR: test_multiline_string_diff
+#[test]
+fn test_multiline_string_diff() {
+    let haiku = "Memory safety found,\n\
+                 Rust's strong typing guides the way,\n\
+                 Secure code you'll write.";
+    assert_that!(
+        haiku,
+        eq("Memory safety found,\n\
+            Rust's silly humor guides the way,\n\
+            Secure code you'll write.")
+    );
+}
+// ANCHOR_END: test_multiline_string_diff
