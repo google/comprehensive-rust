3f7b6d4ad72d08fdb62efe96a01693e6d14c8c58
Ning Chen <87443619+ningcng@users.noreply.github.com>
2023-12-01 02:05:46 -0800

    Fix adb push command (#1542)
    ```
    $ adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust /data/local/tmp"
    adb: push requires <source> and <destination> arguments
    ```
    
    ---------
    
    Co-authored-by: Martin Geisler <mgeisler@google.com>

--- src/android/build_all.sh
+++ src/android/build_all.sh
@@ -62,7 +62,7 @@ adb shell rm -rf '/data/local/tmp/*'
 run_example <<EOF
 # ANCHOR: hello_rust
 m hello_rust
-adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust /data/local/tmp"
+adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust" /data/local/tmp
 adb shell /data/local/tmp/hello_rust
 # ANCHOR_END: hello_rust
 EOF
@@ -70,7 +70,7 @@ EOF
 run_example <<EOF
 # ANCHOR: hello_rust_with_dep
 m hello_rust_with_dep
-adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust_with_dep /data/local/tmp"
+adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust_with_dep" /data/local/tmp
 adb shell /data/local/tmp/hello_rust_with_dep
 # ANCHOR_END: hello_rust_with_dep
 EOF
@@ -79,7 +79,7 @@ function birthday_server() {
   run_example <<EOF
 # ANCHOR: birthday_server
 m birthday_server
-adb push "$ANDROID_PRODUCT_OUT/system/bin/birthday_server /data/local/tmp"
+adb push "$ANDROID_PRODUCT_OUT/system/bin/birthday_server" /data/local/tmp
 adb shell /data/local/tmp/birthday_server
 # ANCHOR_END: birthday_server
 EOF
@@ -110,7 +110,7 @@ EOF
 run_example <<EOF
 # ANCHOR: birthday_client
 m birthday_client
-adb push "$ANDROID_PRODUCT_OUT/system/bin/birthday_client /data/local/tmp"
+adb push "$ANDROID_PRODUCT_OUT/system/bin/birthday_client" /data/local/tmp
 adb shell /data/local/tmp/birthday_client Charlie 60
 # ANCHOR_END: birthday_client
 EOF
@@ -121,7 +121,7 @@ pkill -f birthday_server
 run_example <<EOF
 # ANCHOR: hello_rust_logs
 m hello_rust_logs
-adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust_logs /data/local/tmp"
+adb push "$ANDROID_PRODUCT_OUT/system/bin/hello_rust_logs" /data/local/tmp
 adb shell /data/local/tmp/hello_rust_logs
 # ANCHOR_END: hello_rust_logs
 EOF
@@ -129,7 +129,7 @@ EOF
 run_example <<EOF
 # ANCHOR: print_birthday_card
 m print_birthday_card
-adb push "$ANDROID_PRODUCT_OUT/system/bin/print_birthday_card /data/local/tmp"
+adb push "$ANDROID_PRODUCT_OUT/system/bin/print_birthday_card" /data/local/tmp
 adb shell /data/local/tmp/print_birthday_card
 # ANCHOR_END: print_birthday_card
 EOF
@@ -143,7 +143,7 @@ EOF
 run_example <<EOF
 # ANCHOR: analyze_numbers
 m analyze_numbers
-adb push "$ANDROID_PRODUCT_OUT/system/bin/analyze_numbers /data/local/tmp"
+adb push "$ANDROID_PRODUCT_OUT/system/bin/analyze_numbers" /data/local/tmp
 adb shell /data/local/tmp/analyze_numbers
 # ANCHOR_END: analyze_numbers
 EOF
