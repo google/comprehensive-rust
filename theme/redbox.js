486e01c352b1933f1de8460421e052ce47bf8088
Manichand Kondapaka <mani.chand01@gmail.com>
2024-03-01 10:35:18 +0000

    Added icons as buttons, updated code in redbox, updated content in style.md removed comments about redbox in book.toml and python script.

--- theme/redbox.js
+++ theme/redbox.js
@@ -1,3 +1,4 @@
+(function redBoxButton() {
 // Create a new div element
 var newDiv = document.createElement("div");
 // Set the id attribute of the new div
@@ -12,20 +13,23 @@ var parentElement = document.body; // Change this to your desired parent element
 parentElement.insertBefore(newDiv, parentElement.firstChild);
 // Create the button element
 var hideShowButton = document.createElement("button");
-hideShowButton.innerHTML = "redbox";
+hideShowButton.innerHTML = '<i class=\"fa-solid fa-square\"></i>';
 hideShowButton.className = "icon-button";
 hideShowButton.type = "button";
-hideShowButton.title = "showing/hiding red box";
+hideShowButton.title = "Outline the area that fits on one screen while teaching the course.";
 hideShowButton.id = "Dev";
 var navbarButtons = document.getElementsByClassName("left-buttons");
 navbarButtons[0].insertBefore(hideShowButton, navbarButtons.firstChild);
+//Default hiding the redbox
 document.getElementById("aspect-ratio-helper").style.display = "none";
+//Add Event listener to button to perform on click action.
 hideShowButton.addEventListener("click", function () {
   if (document.getElementById("aspect-ratio-helper").style.display === "none") {
     document.getElementById("aspect-ratio-helper").style.display = "block";
-    hideShowButton.innerHTML = "User";
+    hideShowButton.innerHTML = '<i class=\"fa-thin fa-square\"></i>';
   } else {
     document.getElementById("aspect-ratio-helper").style.display = "none";
     hideShowButton.innerHTML = "Dev";
   }
 });
+})();
