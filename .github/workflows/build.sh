0761f936ca2a98f6aafa6da3c3ecb5778f510166
Manichand Kondapaka <mani.chand01@gmail.com>
2024-03-05 16:03:42 +0530

    Add a dev theme to help with slide aspect ratio (#1842)
    Added a dev theme to help with slide aspect ratio while updating content
    or for reviewing PRs.
    
    Fixes #1796.
    
    ---------
    
    Co-authored-by: Dustin J. Mitchell <djmitche@google.com>
    Co-authored-by: Martin Geisler <martin@geisler.net>

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -37,6 +37,10 @@ else
 fi
 
 mdbook build -d "$dest_dir"
+
+# Disable the redbox button in built versions of the course
+echo '// Disabled in published builds, see build.sh' > "${dest_dir}/html/theme/redbox.js"
+
 mv "$dest_dir/pandoc/pdf/comprehensive-rust.pdf" "$dest_dir/html/"
 (cd "$dest_dir/exerciser" && zip --recurse-paths ../html/comprehensive-rust-exercises.zip comprehensive-rust-exercises/)
 
