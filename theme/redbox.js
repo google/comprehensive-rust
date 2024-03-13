58b36a194a4b87db11556b27562e8cc12485cbfb
Manichand <mani.chand01@gmail.com>
2024-03-13 22:28:49 +0530

    Formatted

--- theme/redbox.js
+++ theme/redbox.js
@@ -39,26 +39,30 @@
 
 function setCodeToPlayground() {
   var codes = JSON.parse(localStorage.getItem(window.location.href));
-  if(codes){
+  if (codes) {
     var i = 0;
-    Array.from(document.querySelectorAll(".playground")).forEach(function (pre_block) {
-    let code_block = pre_block.querySelector("code");
-    let editor = window.ace.edit(code_block);
-    editor.setValue(codes[i]);
-    editor.clearSelection();
-    i += 1
-    })
+    Array.from(document.querySelectorAll(".playground")).forEach(function (
+      pre_block
+    ) {
+      let code_block = pre_block.querySelector("code");
+      let editor = window.ace.edit(code_block);
+      editor.setValue(codes[i]);
+      editor.clearSelection();
+      i += 1;
+    });
   }
 }
 setTimeout(setCodeToPlayground, 100);
 function getCodeFromPlayground() {
-  var codes = []
-  Array.from(document.querySelectorAll(".playground")).forEach(function (pre_block) {
+  var codes = [];
+  Array.from(document.querySelectorAll(".playground")).forEach(function (
+    pre_block
+  ) {
     let code_block = pre_block.querySelector("code");
     let editor = window.ace.edit(code_block);
     let code = editor.getValue();
-    codes.push(code)
-  })
-  localStorage.setItem(window.location.href,JSON.stringify(codes));
-};
-setInterval(getCodeFromPlayground,900)
+    codes.push(code);
+  });
+  localStorage.setItem(window.location.href, JSON.stringify(codes));
+}
+setInterval(getCodeFromPlayground, 900);
