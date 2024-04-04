68ed20953b029c3dcb94dd0d094e6f0a883195ce
Manichand <mani.chand01@gmail.com>
2024-04-04 22:31:15 +0530

    Requested changes

--- theme/save-playgrounds.js
+++ theme/save-playgrounds.js
@@ -1,34 +1,32 @@
 (function savePlaygrounds() {
   function setCodeToPlayground() {
-    var codes = JSON.parse(
-      localStorage.getItem(`${window.location.href}₹code`)
-    );
-    if (codes) {
-      var i = 0;
-      Array.from(document.querySelectorAll(".playground")).forEach(function (
-        pre_block
-      ) {
-        let code_block = pre_block.querySelector("code");
-        let editor = window.ace.edit(code_block);
-        editor.setValue(codes[i]);
+    Array.from(document.querySelectorAll(".playground")).forEach(function (
+      pre_block,
+      index
+    ) {
+      let code_block = pre_block.querySelector("code");
+      let editor = window.ace.edit(code_block);
+      code = JSON.parse(
+        localStorage.getItem(`${window.location.href}₹${index}`)
+      );
+      if (code) {
+        editor.setValue(code);
         editor.clearSelection();
-        i += 1;
-      });
-    }
+      }
+    });
   }
   function getCodeFromPlayground() {
-    var codes = [];
     Array.from(document.querySelectorAll(".playground")).forEach(function (
-      pre_block
+      pre_block,
+      index
     ) {
       let code_block = pre_block.querySelector("code");
       let editor = window.ace.edit(code_block);
       editor.session.on("change", function () {
         let code = editor.getValue();
-        codes.push(code);
         localStorage.setItem(
-          `${window.location.href}₹code`,
-          JSON.stringify(codes)
+          `${window.location.href}₹${index}`,
+          JSON.stringify(code)
         );
       });
     });
@@ -38,9 +36,19 @@
 })();
 
 function resetPlaygroundsClicked() {
+    Array.from(document.querySelectorAll(".playground")).forEach(function (
+    pre_block,
+    index
+  ) {
+    let code_block = pre_block.querySelector("code");
+    let editor = window.ace.edit(code_block);
+    editor.setValue(editor.originalCode)
+    editor.clearSelection();
+  });
+  
   let keys = [];
   for (var i = 0, len = localStorage.length; i < len; i++) {
-    if (localStorage.key(i).includes("₹code")) {
+    if (localStorage.key(i).includes("₹")) {
       keys.push(localStorage.key(i));
     }
   }
