8e668cdaddd68eff8726dcab3781421f09180afd
Dustin J. Mitchell <djmitche@google.com>
2024-05-29 22:15:07 -0400

    Revert "Saving the playground state in local storage (#1942)" (#2106)
    This reverts commit e2cad7da8d85b3d7d783f064b6d4db5b6e7d9580.
    
    Fixes #2105, #2104.

--- theme/save-playgrounds.js
+++ theme/save-playgrounds.js
@@ -1,54 +1,41 @@
 (function savePlaygrounds() {
   function setCodeToPlayground() {
-    Array.from(document.querySelectorAll(".playground")).forEach(function (
-      pre_block,
-      index
-    ) {
-      let code_block = pre_block.querySelector("code");
-      let editor = window.ace.edit(code_block);
-      code = JSON.parse(
-        localStorage.getItem(`${window.location.href}₹${index}`)
-      );
-      if (code) {
-        editor.setValue(code);
+    var codes = JSON.parse(
+      localStorage.getItem(`${window.location.href}₹code`)
+    );
+    if (codes) {
+      var i = 0;
+      Array.from(document.querySelectorAll(".playground")).forEach(function (
+        pre_block
+      ) {
+        let code_block = pre_block.querySelector("code");
+        let editor = window.ace.edit(code_block);
+        editor.setValue(codes[i]);
         editor.clearSelection();
-      }
-    });
+        i += 1;
+      });
+    }
   }
   function getCodeFromPlayground() {
+    var codes = [];
     Array.from(document.querySelectorAll(".playground")).forEach(function (
-      pre_block,
-      index
+      pre_block
     ) {
       let code_block = pre_block.querySelector("code");
       let editor = window.ace.edit(code_block);
-      editor.session.on("change", function () {
-        let code = editor.getValue();
-        localStorage.setItem(
-          `${window.location.href}₹${index}`,
-          JSON.stringify(code)
-        );
-      });
+      let code = editor.getValue();
+      codes.push(code);
     });
+    localStorage.setItem(`${window.location.href}₹code`, JSON.stringify(codes));
   }
   setCodeToPlayground();
-  getCodeFromPlayground();
+  addEventListener("pagehide", getCodeFromPlayground);
 })();
 
 function resetPlaygroundsClicked() {
-  Array.from(document.querySelectorAll(".playground")).forEach(function (
-    pre_block,
-    index
-  ) {
-    let code_block = pre_block.querySelector("code");
-    let editor = window.ace.edit(code_block);
-    editor.setValue(editor.originalCode);
-    editor.clearSelection();
-  });
-
   let keys = [];
   for (var i = 0, len = localStorage.length; i < len; i++) {
-    if (localStorage.key(i).includes("₹")) {
+    if (localStorage.key(i).includes("₹code")) {
       keys.push(localStorage.key(i));
     }
   }
