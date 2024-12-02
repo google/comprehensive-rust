f69aa9d0a7c3c059f6058e405a4fc716bb27905a
Michael Kerscher <kerscher@google.com>
2024-12-02 10:11:48 +0100

    tests: set logLevel to warn and define a default script run by "npm start" and "npm test"

--- tests/wdio.conf.ts
+++ tests/wdio.conf.ts
@@ -68,7 +68,7 @@ export const config: WebdriverIO.Config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: "info",
+  logLevel: "warn",
   //
   // Set specific log levels per logger
   // loggers:
