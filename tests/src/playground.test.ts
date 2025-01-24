d5208f9a0a4d85161bb7c0026bd7b5fbf20b26c7
Michael Kerscher <kerscher@google.com>
2025-01-24 17:18:08 +0100

    tests: improve the playground test by mocking responses

--- tests/src/playground.test.ts
+++ tests/src/playground.test.ts
@@ -1,59 +1,49 @@
 import { describe, it } from "mocha";
-import { $, expect, browser } from "@wdio/globals";
+import { expect, browser } from "@wdio/globals";
 import { Key } from "webdriverio";
+import Playground from "./objects/playground";
 
 describe("Playground", () => {
-  it("executes the hello world code and prints the hello message", async () => {
+  beforeEach(async () => {
+    // this page contains a hello world playground
     await browser.url("/hello-world/hello-world.html");
-    const playground_area = $(".ace_content");
-    const playground_start_button = $("button.play-button");
-    const playground_stderr = $("code.result.stderr");
-    const playground_stdout = $("code.result.stdout");
+  });
 
+  it("exists and is shown but output fields do not yet exist", async () => {
+    const playground = new Playground();
     // ensure a playground exists and pre-state is as expected
-    await expect(playground_area).toExist();
-    await expect(playground_start_button).toExist();
-    await expect(playground_start_button).not.toBeDisplayed();
-    await expect(playground_stderr).not.toExist();
-    await expect(playground_stdout).not.toExist();
+    await expect(playground.area).toExist();
+    await expect(playground.start_button).toExist();
+    await expect(playground.start_button).not.toBeDisplayed();
+    await expect(playground.stderr).not.toExist();
+    await expect(playground.stdout).not.toExist();
+  });
 
-    // clicking into the content is necessary for the button to be displayed
-    await playground_area.waitForClickable();
-    await playground_area.click();
-    await expect(playground_start_button).toBeDisplayed();
+  it("executes the hello world code and prints the hello message", async () => {
+    const playground = new Playground();
+    await playground.mock_success("Hello world!\n");
+    await playground.activate();
+    await playground.run();
 
-    // clicking the button triggers action
-    await playground_start_button.waitForClickable();
-    await playground_start_button.click();
-    await expect(playground_stdout).toBeDisplayed();
-    await expect(playground_stderr).not.toBeDisplayed();
-    await expect(playground_stdout).toHaveText(expect.stringContaining("🌍"));
+    await expect(playground.stdout).toBeDisplayed();
+    await expect(playground.stderr).not.toBeDisplayed();
+    await expect(playground.stdout).toHaveText("Hello world!");
   });
 
   it("shows error messages in stderr if the code is broken", async () => {
-    await browser.url("/hello-world/hello-world.html");
-    const playground_area = $(".ace_content");
-    const playground_start_button = $("button.play-button");
-    const playground_stderr = $("code.result.stderr");
-    const playground_stdout = $("code.result.stdout");
-
-    // clicking into the content is necessary for the button to be displayed
-    await playground_area.waitForClickable();
-    await playground_area.click();
-
+    const playground = new Playground();
+    await playground.mock_fail();
+    await playground.activate();
     // append some failing code to the editor that is now in focus
     await browser.keys([Key.Enter, "fn expect_failure()"]);
+    await playground.run();
 
-    // clicking the button triggers action
-    await playground_start_button.waitForClickable();
-    await playground_start_button.click();
-    await expect(playground_stdout).toBeDisplayed();
-    await expect(playground_stderr).toBeDisplayed();
-
+    await expect(playground.stdout).toBeDisplayed();
+    await expect(playground.stderr).toBeDisplayed();
     // check for error message in stderr
-    await expect(playground_stderr).toHaveText(
+    await expect(playground.stderr).toHaveText(
       expect.stringContaining("error: could not compile")
     );
-    await expect(playground_stdout).toHaveText("No output");
+    await expect(playground.stdout).toHaveText("No output");
   });
 });
