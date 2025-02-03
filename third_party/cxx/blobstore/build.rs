43e1cd6c49c6b50609bb7c827466af4a3bfb1d24
Frances Wingerter <91758128+fw-immunant@users.noreply.github.com>
2025-02-03 17:23:35 +0000

    cxx: fix build with CARGO_TARGET_DIR set (#2542)
    Fixes #2335.

--- third_party/cxx/blobstore/build.rs
+++ third_party/cxx/blobstore/build.rs
@@ -1,9 +1,13 @@
 fn main() {
+    // Find target directory, either from CARGO_TARGET_DIR or in-tree if unset.
+    let mut src_dir = std::env::var_os("CARGO_TARGET_DIR").unwrap_or("../../../target".into());
+    src_dir.push("/cxxbridge/demo/src");
+
     cxx_build::bridge("src/main.rs")
         .file("src/blobstore.cc")
         .flag_if_supported("-std=c++14")
         .include(".")
-        .include("../../../target/cxxbridge/demo/src")
+        .include(src_dir)
         .compile("cxxbridge-demo");
 
     println!("cargo:rerun-if-changed=src/main.rs");
