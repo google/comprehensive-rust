645c5d3ea2e56ee10f08a24e6f118852a5011229
Eric Githinji <egithinji@google.com>
2025-03-03 06:31:41 +0000

    Use one cross-platform command to install tools.
    This follows the approach in https://github.com/matklad/cargo-xtask and
    allow us to run `cargo xtask install-tools` from the project root to
    install the tools that comprehensive rust depends on, rather than
    using a bash script which is not truly cross platform.

---
+++ xtask/src/main.rs
@@ -0,0 +1,48 @@
+use std::{
+    env,
+    process::Command,
+};
+
+type DynError = Box<dyn std::error::Error>;
+
+fn main() {
+    if let Err(e) = execute_task()  {
+        eprintln!("{e}");
+       std::process::exit(-1);
+    }
+}
+
+fn execute_task() -> Result<(), DynError> {
+    let task = env::args().nth(1);
+    match task.as_deref() {
+        Some("install-tools") => install_tools()?,
+        _ => print_help(task.as_deref()),
+    }
+    Ok(())
+}
+
+fn install_tools() -> Result<(), DynError>{
+    println!("Installing project tools...");
+    Command::new(env!("CARGO")).arg("install").args(["mdbook", "--locked", "--version", "0.4.44"]).status()?;
+    Command::new(env!("CARGO")).arg("install").args(["mdbook-svgbob", "--locked", "--version", "0.2.1"]).status()?;
+    Command::new(env!("CARGO")).arg("install").args(["mdbook-pandoc", "--locked", "--version", "0.9.3"]).status()?;
+    Command::new(env!("CARGO")).arg("install").args(["mdbook-i18n-helpers", "--locked", "--version", "0.3.5"]).status()?;
+    Command::new(env!("CARGO")).arg("install").args(["i18n-report", "--locked", "--version", "0.2.0"]).status()?;
+    Command::new(env!("CARGO")).arg("install").args(["--path", "mdbook-exerciser", "--locked"]).status()?;
+    Command::new(env!("CARGO")).arg("install").args(["--path", "mdbook-course", "--locked"]).status()?;
+    Ok(())
+}
+
+fn print_help(task: Option<&str>) {
+    if let Some(t) = task {
+    eprintln!(
+        "Unrecognized task '{t}'. Available tasks:
+
+install-tools            Installs the tools the project dependends on.
+
+Run with `cargo xtask [task]`.
+");
+    } else {
+        eprintln!("Missing task. To execute a task run `cargo xtask [task]`.");
+    }
+}
