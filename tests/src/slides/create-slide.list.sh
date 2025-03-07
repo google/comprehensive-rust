d1853efa9e7e090939817e7be477f7837b894dfd
Michael Kerscher <kerscher@google.com>
2025-03-07 16:49:35 +0100

    tests: add create slide list script to CI and remove generated list. This ensures new slides are checked. Developers can create them manually.

--- tests/src/slides/create-slide.list.sh
+++ tests/src/slides/create-slide.list.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-# This script recreates the slides.list.ts file based on the given book html directory.
+# This script (re)creates the slides.list.ts file based on the given book html directory.
 # It is used to regenerate the list of slides that are tested in the slide-size.test.ts file.
+# Takes either TEST_BOOK_DIR environment variable or first parameter as override.
 
 set -e
 BASEDIR="$(dirname "$0")"
 
-if [[ "$1" == "" ]] ; then
-  echo "Usage: $0 <book_dir>"
+if [[ -n "$1" ]]; then
+    # take directory from command line
+    TEST_BOOK_DIR="$1"
+fi
+
+# check if TEST_BOOK_DIR is empty (not set by environment nor parameter)
+if [[ -z "${TEST_BOOK_DIR}" ]]; then
+  echo "Usage: $0 <book_html_dir>"
   exit 1
 fi
-BOOKDIR="$1"
 
 pushd "${TEST_BOOK_DIR}"
 # exclude special pages that should never be tested
