a098835cd6f50c4179bf9397dc64c49b9eed6720
Eric Githinji <egithinji@google.com>
2025-05-06 06:30:57 +0000

    Formatting.

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -21,8 +21,8 @@
 
 use anyhow::{anyhow, Ok, Result};
 use clap::Parser;
-use std::{env, process::Command};
 use std::path::Path;
+use std::{env, process::Command};
 
 fn main() -> Result<()> {
     if let Err(e) = execute_task() {
