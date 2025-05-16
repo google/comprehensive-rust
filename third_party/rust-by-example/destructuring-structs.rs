dbd1a09031714bda7be972ea14686f44d8cbe451
Nicole L <dlegare.1001@gmail.com>
2025-05-16 14:10:30 -0700

    Reorder struct pattern examples (#2749)
    I generally like to start with that second case since imo it's the
    simplest one before going on to the case that uses a tuple sub-pattern
    to break up the `x` field.

--- third_party/rust-by-example/destructuring-structs.rs
+++ third_party/rust-by-example/destructuring-structs.rs
@@ -7,8 +7,8 @@ struct Foo {
 fn main() {
     let foo = Foo { x: (1, 2), y: 3 };
     match foo {
-        Foo { x: (1, b), y } => println!("x.0 = 1, b = {b}, y = {y}"),
         Foo { y: 2, x: i }   => println!("y = 2, x = {i:?}"),
+        Foo { x: (1, b), y } => println!("x.0 = 1, b = {b}, y = {y}"),
         Foo { y, .. }        => println!("y = {y}, other fields were ignored"),
     }
 }
