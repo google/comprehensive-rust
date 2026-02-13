dbbbfbaa2491062c3170cdf9a0c08c32d984ed43
Dmitri Gribenko <gribozavr@gmail.com>
2026-02-14 00:39:03 +0100

    build: add SPDX-License-Identifier to Apache 2.0 licensed files
    This commit adds the `SPDX-License-Identifier: Apache-2.0` header to all
    source files that currently contain the Apache 2.0 license boilerplate.
    This makes the licensing information machine-readable and compliant with
    SPDX standards.

--- src/android/bpfmt.sh
+++ src/android/bpfmt.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 # Copyright 2022 Google LLC
+# SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
